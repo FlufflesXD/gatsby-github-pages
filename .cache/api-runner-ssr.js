3bdfb444fbbb89c65372da53999b9359a55713f2
FlufflesXD <124913599+FlufflesXD@users.noreply.github.com>
2023-12-15 14:39:59 -0500

    first push of public info

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -13,7 +13,7 @@ var plugins = [{
     },{
       name: 'gatsby-plugin-manifest',
       plugin: require('C:/Users/Maks Venne/Valley-East-Martial-Arts-Site/node_modules/gatsby-plugin-manifest/gatsby-ssr.js'),
-      options: {"plugins":[],"name":"My Minimal Portfolio","short_name":"Portfolio","start_url":"/","background_color":"#FFFFFF","theme_color":"#000000","display":"minimal-ui","icon":"./content/images/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"d48eafceaf44a7dcd7ae15ab27bfe91b"},
+      options: {"plugins":[],"name":"Vefmac","short_name":"Vefmac","start_url":"/","background_color":"#FFFFFF","theme_color":"#000000","display":"minimal-ui","icon":"./content/images/favicon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"06ebb80902b2c27b0e63923abd60e49b"},
     },{
       name: 'gatsby-plugin-offline',
       plugin: require('C:/Users/Maks Venne/Valley-East-Martial-Arts-Site/node_modules/gatsby-plugin-offline/gatsby-ssr.js'),
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/gatsby-github-pages",
   plugins: [
     {
       resolve: "gatsby-theme-portfolio-minimal",
