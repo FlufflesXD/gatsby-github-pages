111d746475548f6670e1241cb913d49046a0e4af
FlufflesXD <124913599+FlufflesXD@users.noreply.github.com>
2023-12-15 17:10:07 -0500

    please work

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("C:\\Users\\Maks Venne\\Valley-East-Martial-Arts-Site\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Maks Venne\\Valley-East-Martial-Arts-Site\\.cache\\dev-404-page.js")),
   "component---node-modules-gatsby-theme-portfolio-minimal-src-pages-404-tsx": preferDefault(require("C:\\Users\\Maks Venne\\Valley-East-Martial-Arts-Site\\node_modules\\gatsby-theme-portfolio-minimal\\src\\pages\\404.tsx")),
   "component---node-modules-gatsby-theme-portfolio-minimal-src-templates-article-index-tsx": preferDefault(require("C:\\Users\\Maks Venne\\Valley-East-Martial-Arts-Site\\node_modules\\gatsby-theme-portfolio-minimal\\src\\templates\\Article\\index.tsx")),
   "component---node-modules-gatsby-theme-portfolio-minimal-src-templates-article-listing-index-tsx": preferDefault(require("C:\\Users\\Maks Venne\\Valley-East-Martial-Arts-Site\\node_modules\\gatsby-theme-portfolio-minimal\\src\\templates\\ArticleListing\\index.tsx")),
